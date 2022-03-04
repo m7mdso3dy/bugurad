bbbadb8c3c209436e987c95edec8994e36fd1a27
m7mdso3dy <kimosody125@gmail.com>
2022-03-04 14:45:17 +0200

    under construction page

--- drop-down.js
+++ drop-down.js
@@ -1,1 +1,14 @@
-document.querySelector('header section p').innerHTML = 'Discover your weakness before they do'
+document.querySelector('header section p').innerHTML = 'Discover your weakness before they do';
+
+const links = document.querySelectorAll('nav a');
+links.forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        console.log(this)
+        if (this.href.includes('#')) {
+            location.replace('/underconstruction.html')
+        } else {
+            location.replace(this.href)
+        }
+    });
+})
